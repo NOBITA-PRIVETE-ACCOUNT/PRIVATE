b19512848b614c677e995d312288fe26b5ead9ee
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2024-10-27 02:15:35 +0530

    Create __init__.py

---
+++ NOBITAMUSIC/utils/inline/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2024 by VISHAL-PANDEY@Github, < https://github.com/vishalpandeynkp1 >.
+#
+# This file is part of < https://github.com/vishalpandeynkp1/VIPNOBITAMUSIC_REPO > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/vishalpandeynkp1/VIPNOBITAMUSIC_REPO/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+from .help import *
+from .play import *
+from .playlist import *
+from .queue import *
+from .settings import *
+from .song import *
+from .speed import *
+from .start import *
